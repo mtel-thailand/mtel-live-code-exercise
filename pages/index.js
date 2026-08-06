7b2b71fda830edaad8c91e3af5f2068915ef6b46
Kong <athichart.chu@mtel.co.th>
2026-08-06 17:29:17 +0700

    chore: remove log

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,6 @@ export default function Home() {
 
         <ul className="list">
           {products.map((product) => {
-            console.log("🚀 ~ Home ~ product:", product)
             return <li key={product.id} className="row">
               <span className="name">{product.name}</span>
               <span className="category">{product.categoryName}</span>
